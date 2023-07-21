92dbf4dd90b54c44caf1a05b868e49ef95750014
Lyove <lyove@outlook.com>
2023-07-21 10:19:58 +0800

    init

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,55 +1,65 @@
-import { PLACEMENT } from "./constant";
 import type { Position, Rect, TransitionInfo } from "./type";
+import { PLACEMENT } from "./constant";
 
-function getPopOffset(position: PLACEMENT, triggerRect: Rect, popWH: Rect, translate: number[]) {
+function getPopoverOffset({
+  position,
+  triggerRect,
+  popoverRect,
+  translate,
+}: {
+  position: PLACEMENT;
+  triggerRect: Rect;
+  popoverRect: Rect;
+  translate: number[];
+}) {
   switch (position) {
     case PLACEMENT.T:
       return [
-        triggerRect.left + triggerRect.width / 2 - popWH.width / 2 + translate[0],
-        triggerRect.top - popWH.height + translate[1],
+        triggerRect.left + triggerRect.width / 2 - popoverRect.width / 2 + translate[0],
+        triggerRect.top - popoverRect.height + translate[1],
       ];
     case PLACEMENT.TL:
-      return [triggerRect.left + translate[0], triggerRect.top - popWH.height + translate[1]];
+      return [triggerRect.left + translate[0], triggerRect.top - popoverRect.height + translate[1]];
     case PLACEMENT.TR:
       return [
-        triggerRect.left + triggerRect.width - popWH.width + translate[0],
-        triggerRect.top - popWH.height + translate[1],
+        triggerRect.left + triggerRect.width - popoverRect.width + translate[0],
+        triggerRect.top - popoverRect.height + translate[1],
       ];
     case PLACEMENT.B:
       return [
-        triggerRect.left + triggerRect.width / 2 - popWH.width / 2 + translate[0],
+        triggerRect.left + triggerRect.width / 2 - popoverRect.width / 2 + translate[0],
         triggerRect.top + triggerRect.height + translate[1],
       ];
     case PLACEMENT.BL:
       return [triggerRect.left + translate[0], triggerRect.top + triggerRect.height + translate[1]];
     case PLACEMENT.BR:
       return [
-        triggerRect.left + triggerRect.width - popWH.width + translate[0],
+        triggerRect.left + triggerRect.width - popoverRect.width + translate[0],
         triggerRect.top + triggerRect.height + translate[1],
       ];
     case PLACEMENT.L:
       return [
-        triggerRect.left - popWH.width + translate[0],
-        triggerRect.top + triggerRect.height / 2 - popWH.height / 2 + translate[1],
+        triggerRect.left - popoverRect.width + translate[0],
+        triggerRect.top + triggerRect.height / 2 - popoverRect.height / 2 + translate[1],
       ];
     case PLACEMENT.LT:
-      return [triggerRect.left - popWH.width + translate[0], triggerRect.top + translate[1]];
+      return [triggerRect.left - popoverRect.width + translate[0], triggerRect.top + translate[1]];
     case PLACEMENT.LB:
       return [
-        triggerRect.left - popWH.width + translate[0],
-        triggerRect.top + triggerRect.height - popWH.height + translate[1],
+        triggerRect.left - popoverRect.width + translate[0],
+        triggerRect.top + triggerRect.height - popoverRect.height + translate[1],
       ];
     case PLACEMENT.R:
       return [
         triggerRect.left + triggerRect.width + translate[0],
-        triggerRect.top + triggerRect.height / 2 - popWH.height / 2 + translate[1],
+        triggerRect.top + triggerRect.height / 2 - popoverRect.height / 2 + translate[1],
       ];
     case PLACEMENT.RT:
       return [triggerRect.left + triggerRect.width + translate[0], triggerRect.top + translate[1]];
     case PLACEMENT.RB:
       return [
         triggerRect.left + triggerRect.width + translate[0],
-        triggerRect.top + triggerRect.height - popWH.height + translate[1],
+        triggerRect.top + triggerRect.height - popoverRect.height + translate[1],
       ];
     default:
       return [0, 0];
@@ -133,173 +143,186 @@ function changePosition(
   }
 }
 
-function getFitPosition(
-  position: PLACEMENT,
-  popoverPosition: number[],
-  containerRect: Rect,
-  popWH: Rect,
-  triggerRect: Rect,
-  translate: number[],
-  direction: PLACEMENT,
-  overflow?: boolean,
-) {
+function getFitPosition({
+  position,
+  popoverPosition,
+  triggerRect,
+  popoverRect,
+  mountContainerRect,
+  translate,
+  direction,
+  overflow,
+}: {
+  position: PLACEMENT;
+  popoverPosition: number[];
+  triggerRect: Rect;
+  popoverRect: Rect;
+  mountContainerRect: Rect;
+  translate: number[];
+  direction: PLACEMENT;
+  overflow?: boolean;
+}) {
   const viewPortSize = [
     document.documentElement.clientWidth || window.innerWidth,
     document.documentElement.clientHeight || window.innerHeight,
   ];
 
   const boundary = [
-    overflow ? Math.max(containerRect.left, 0) : 0,
-    overflow ? Math.max(containerRect.top, 0) : 0,
+    overflow ? Math.max(mountContainerRect.left, 0) : 0,
+    overflow ? Math.max(mountContainerRect.top, 0) : 0,
     overflow
-      ? Math.min(containerRect.left + containerRect.width, viewPortSize[0])
+      ? Math.min(mountContainerRect.left + mountContainerRect.width, viewPortSize[0])
       : viewPortSize[0],
     overflow
-      ? Math.min(containerRect.top + containerRect.height, viewPortSize[1])
+      ? Math.min(mountContainerRect.top + mountContainerRect.height, viewPortSize[1])
       : viewPortSize[1],
   ];
-  const x = containerRect.left + popoverPosition[0];
-  const y = containerRect.top + popoverPosition[1];
+  const x = mountContainerRect.left + popoverPosition[0];
+  const y = mountContainerRect.top + popoverPosition[1];
   const popRect = [
-    containerRect.left + popoverPosition[0],
-    containerRect.top + popoverPosition[1],
-    x + popWH.width,
-    y + popWH.height,
+    mountContainerRect.left + popoverPosition[0],
+    mountContainerRect.top + popoverPosition[1],
+    x + popoverRect.width,
+    y + popoverRect.height,
   ];
-  const triggerX = containerRect.left + triggerRect.left;
-  const triggerY = containerRect.top + triggerRect.top;
+  const triggerX = mountContainerRect.left + triggerRect.left;
+  const triggerY = mountContainerRect.top + triggerRect.top;
   const triggerEx = triggerX + triggerRect.width;
   const triggerEy = triggerY + triggerRect.height;
   let finalPosition = position;
   if (direction === PLACEMENT.T) {
     if (y < boundary[1]) {
       if (
-        boundary[3] - triggerEy + translate[1] >= popWH.height &&
+        boundary[3] - triggerEy + translate[1] >= popoverRect.height &&
         triggerEy - translate[1] >= boundary[1]
       ) {
-        popoverPosition[1] = getPopOffset(PLACEMENT.B, triggerRect, popWH, [
-          translate[0],
-          -translate[1],
-        ])[1];
+        popoverPosition[1] = getPopoverOffset({
+          position: PLACEMENT.B,
+          triggerRect: triggerRect,
+          popoverRect,
+          translate: [translate[0], -translate[1]],
+        })[1];
         finalPosition = changePosition(position, PLACEMENT.B);
       } else {
-        popoverPosition[1] = overflow ? 0 : -containerRect.top;
+        popoverPosition[1] = overflow ? 0 : -mountContainerRect.top;
       }
     } else if (popRect[3] > boundary[3]) {
       popoverPosition[1] = overflow
-        ? containerRect.height - popWH.height
-        : viewPortSize[1] - containerRect.top - popWH.height;
+        ? mountContainerRect.height - popoverRect.height
+        : viewPortSize[1] - mountContainerRect.top - popoverRect.height;
     }
   } else if (direction === PLACEMENT.B) {
     if (popRect[3] > boundary[3]) {
       if (
-        triggerY - boundary[1] - translate[1] >= popWH.height &&
+        triggerY - boundary[1] - translate[1] >= popoverRect.height &&
         triggerY - translate[1] <= boundary[3]
       ) {
-        popoverPosition[1] = getPopOffset(PLACEMENT.T, triggerRect, popWH, [
-          translate[0],
-          -translate[1],
-        ])[1];
+        popoverPosition[1] = getPopoverOffset({
+          position: PLACEMENT.T,
+          triggerRect,
+          popoverRect,
+          translate: [translate[0], -translate[1]],
+        })[1];
         finalPosition = changePosition(position, PLACEMENT.T);
       } else {
         popoverPosition[1] = overflow
-          ? containerRect.height - popWH.height
-          : viewPortSize[1] - containerRect.top - popWH.height;
+          ? mountContainerRect.height - popoverRect.height
+          : viewPortSize[1] - mountContainerRect.top - popoverRect.height;
       }
     } else if (y < boundary[1]) {
-      popoverPosition[1] = overflow ? 0 : -containerRect.top;
+      popoverPosition[1] = overflow ? 0 : -mountContainerRect.top;
     }
   } else if (direction === PLACEMENT.L) {
     if (x < boundary[0]) {
       if (
-        boundary[2] - triggerEx + translate[0] >= popWH.width &&
+        boundary[2] - triggerEx + translate[0] >= popoverRect.width &&
         triggerEx - translate[0] >= boundary[0]
       ) {
         finalPosition = changePosition(position, PLACEMENT.R);
-        popoverPosition[0] = getPopOffset(PLACEMENT.R, triggerRect, popWH, [
-          -translate[0],
-          translate[1],
-        ])[0];
+        popoverPosition[0] = getPopoverOffset({
+          position: PLACEMENT.R,
+          triggerRect,
+          popoverRect,
+          translate: [translate[0], -translate[1]],
+        })[0];
       } else {
-        popoverPosition[0] = overflow ? 0 : -containerRect.left;
+        popoverPosition[0] = overflow ? 0 : -mountContainerRect.left;
       }
     } else if (popRect[2] > boundary[2]) {
       popoverPosition[0] = overflow
-        ? containerRect.width - popWH.width
-        : viewPortSize[0] - containerRect.left + popWH.width;
+        ? mountContainerRect.width - popoverRect.width
+        : viewPortSize[0] - mountContainerRect.left + popoverRect.width;
     }
   } else if (direction === PLACEMENT.R) {
     if (popRect[2] > boundary[2]) {
       if (
-        triggerX - boundary[0] - translate[0] >= popWH.width &&
+        triggerX - boundary[0] - translate[0] >= popoverRect.width &&
         triggerX - translate[0] <= boundary[2]
       ) {
         finalPosition = changePosition(position, PLACEMENT.L);
-        popoverPosition[0] = getPopOffset(PLACEMENT.L, triggerRect, popWH, [
-          -translate[0],
-          translate[1],
-        ])[0];
+        popoverPosition[0] = getPopoverOffset({
+          position: PLACEMENT.L,
+          triggerRect,
+          popoverRect,
+          translate: [translate[0], -translate[1]],
+        })[0];
       } else {
         popoverPosition[0] = overflow
-          ? containerRect.width - popWH.width
-          : viewPortSize[0] - containerRect.left + popWH.width;
+          ? mountContainerRect.width - popoverRect.width
+          : viewPortSize[0] - mountContainerRect.left + popoverRect.width;
       }
     } else if (x < boundary[0]) {
-      popoverPosition[0] = overflow ? 0 : -containerRect.left;
+      popoverPosition[0] = overflow ? 0 : -mountContainerRect.left;
     }
   }
 
   if (direction === PLACEMENT.T || direction === PLACEMENT.B) {
     if (x < boundary[0]) {
-      popoverPosition[0] = overflow ? 0 : -containerRect.left;
+      popoverPosition[0] = overflow ? 0 : -mountContainerRect.left;
     } else if (popRect[2] > boundary[2]) {
       popoverPosition[0] = overflow
-        ? containerRect.width - popWH.width
-        : viewPortSize[0] - containerRect.left + popWH.width;
+        ? mountContainerRect.width - popoverRect.width
+        : viewPortSize[0] - mountContainerRect.left + popoverRect.width;
     }
   } else if (direction === PLACEMENT.L || direction === PLACEMENT.R) {
     if (y < boundary[1]) {
-      popoverPosition[1] = overflow ? 0 : -containerRect.top;
+      popoverPosition[1] = overflow ? 0 : -mountContainerRect.top;
     } else if (popRect[3] > boundary[3]) {
       popoverPosition[1] = overflow
-        ? containerRect.height - popWH.height
-        : viewPortSize[1] - containerRect.top - popWH.height;
+        ? mountContainerRect.height - popoverRect.height
+        : viewPortSize[1] - mountContainerRect.top - popoverRect.height;
     }
   }
 
   return finalPosition;
 }
 
-function isScrollElement(element: HTMLElement) {
-  return element.scrollHeight > element.offsetHeight || element.scrollWidth > element.offsetWidth;
-}
-
-function toMs(s: string): number {
-  return Number(s.slice(0, -1).replace(",", ".")) * 1000;
-}
-
-function getTimeout(delays: string[], durations: string[]): number {
-  while (delays.length < durations.length) {
-    delays = delays.concat(delays);
-  }
-  return Math.max(...durations.map((d, i) => toMs(d) + toMs(delays[i])));
-}
-
-export function getPopStyle(
-  position: PLACEMENT,
-  containerRect: Rect,
-  triggerRect: Rect,
-  popWH: Rect,
-  translate: number[],
+export function getPopoverStyle({
+  position,
+  triggerRect,
+  popoverRect,
+  arrowRect,
+  mountContainerRect,
+  translate,
   autoFit = true,
-  overflow?: boolean,
-  coverTrigger?: boolean,
-  arrowWH?: Rect,
-  hideOnInvisible?: boolean,
-): Position {
+  coverTrigger,
+  hideOnInvisible,
+  overflow,
+}: {
+  position: PLACEMENT;
+  triggerRect: Rect;
+  popoverRect: Rect;
+  arrowRect?: Rect;
+  mountContainerRect: Rect;
+  translate: number[];
+  autoFit: boolean;
+  coverTrigger?: boolean;
+  hideOnInvisible?: boolean;
+  overflow?: boolean;
+}): Position {
   const triggerOut =
-    triggerRect.left >= containerRect.width ||
-    triggerRect.top >= containerRect.height ||
+    triggerRect.left >= mountContainerRect.width ||
+    triggerRect.top >= mountContainerRect.height ||
     triggerRect.left + triggerRect.width <= 0 ||
     triggerRect.top + triggerRect.height <= 0;
 
@@ -307,55 +330,60 @@ export function getPopStyle(
     return { position };
   }
 
-  let der = getBoundaryPosition(position);
+  let direction = getBoundaryPosition(position);
   if (coverTrigger) {
     translate = [...translate];
-    if (der === PLACEMENT.T) {
+    if (direction === PLACEMENT.T) {
       translate[1] += triggerRect.height;
-    } else if (der === PLACEMENT.B) {
+    } else if (direction === PLACEMENT.B) {
       translate[1] -= triggerRect.height;
-    } else if (der === PLACEMENT.L) {
+    } else if (direction === PLACEMENT.L) {
       translate[0] += triggerRect.width;
     } else {
       translate[0] -= triggerRect.width;
     }
   }
 
-  const popoverPosition = getPopOffset(position, triggerRect, popWH, translate);
+  const popoverPosition = getPopoverOffset({
+    position,
+    triggerRect,
+    popoverRect,
+    translate,
+  });
 
   if (autoFit) {
-    position = getFitPosition(
+    position = getFitPosition({
       position,
       popoverPosition,
-      containerRect,
-      popWH,
       triggerRect,
+      popoverRect,
+      mountContainerRect,
       translate,
-      der,
+      direction,
       overflow,
-    );
+    });
   }
 
   let arrowXY: undefined | number[];
-  if (!triggerOut && arrowWH) {
-    der = getBoundaryPosition(position);
+  if (!triggerOut && arrowRect) {
+    direction = getBoundaryPosition(position);
     arrowXY = [];
-    const half = [arrowWH.width / 2, arrowWH.height / 2];
-    const isL = der === PLACEMENT.L;
-    const isR = der === PLACEMENT.R;
+    const half = [arrowRect.width / 2, arrowRect.height / 2];
+    const isL = direction === PLACEMENT.L;
+    const isR = direction === PLACEMENT.R;
     if (isL || isR) {
       arrowXY[1] = triggerRect.top + triggerRect.height / 2 - popoverPosition[1] - half[1];
-      if (arrowXY[1] < half[1] || arrowXY[1] > popWH.height - arrowWH.height - half[1]) {
+      if (arrowXY[1] < half[1] || arrowXY[1] > popoverRect.height - arrowRect.height - half[1]) {
         arrowXY = undefined;
       } else {
-        arrowXY[0] = (isL ? popWH.width : 0) - half[0];
+        arrowXY[0] = (isL ? popoverRect.width : 0) - half[0];
       }
     } else {
       arrowXY[0] = triggerRect.left + triggerRect.width / 2 - popoverPosition[0] - half[0];
-      if (arrowXY[0] < half[0] || arrowXY[0] > popWH.width - arrowWH.width - half[0]) {
+      if (arrowXY[0] < half[0] || arrowXY[0] > popoverRect.width - arrowRect.width - half[0]) {
         arrowXY = undefined;
       } else {
-        arrowXY[1] = (der === PLACEMENT.T ? popWH.height : 0) - half[1];
+        arrowXY[1] = (direction === PLACEMENT.T ? popoverRect.height : 0) - half[1];
       }
     }
   }
@@ -370,6 +398,15 @@ export function getPopStyle(
 export function getTransitionInfo(el: Element): TransitionInfo {
   const styles = window.getComputedStyle(el);
   const getStyleProperties = (key: keyof typeof styles) => ((styles[key] || "") as any).split(", ");
+  const getTimeout = (delays: string[], durations: string[]): number => {
+    const toMs = (s: string): number => {
+      return Number(s.slice(0, -1).replace(",", ".")) * 1000;
+    };
+    while (delays.length < durations.length) {
+      delays = delays.concat(delays);
+    }
+    return Math.max(...durations.map((d, i) => toMs(d) + toMs(delays[i])));
+  };
   const transitionDelays = getStyleProperties("transitionDelay");
   const transitionDurations = getStyleProperties("transitionDuration");
   const transitionTimeout = getTimeout(transitionDelays, transitionDurations);
@@ -390,21 +427,18 @@ export function getTransitionInfo(el: Element): TransitionInfo {
   };
 }
 
-export function getScrollElements(el: HTMLElement, container: HTMLElement) {
+export function getScrollElements(el: HTMLElement, mountContainer: HTMLElement) {
   const scrollElements: HTMLElement[] = [];
-  while (el && el !== container) {
+  const isScrollElement = (element: HTMLElement) => {
+    return element.scrollHeight > element.offsetHeight || element.scrollWidth > element.offsetWidth;
+  };
+  while (el && el !== mountContainer) {
     if (isScrollElement(el)) {
       scrollElements.push(el);
     }
     el = el.parentElement!;
   }
   return scrollElements;
-}
-
-export function isElementClipped(element: Element) {
-  const { overflow, overflowX, overflowY } = window.getComputedStyle(element);
-  const o = overflow + overflowY + overflowX;
-  return o.includes("hidden") || o.includes("clip");
 }
 
 export function showDomElement(el: HTMLElement) {

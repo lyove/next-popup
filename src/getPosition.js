43f388639f60495a6486b3b99366dc2fd994f2a0
Lyove <lyove@outlook.com>
2023-08-29 17:20:40 +0800

    init

--- src/getPosition.js
+++ src/getPosition.js
@@ -1,302 +1,61 @@
-export class DomUtils {
-  // Add class to the specified element
-  static addClass($element, className) {
-    if (!$element) {
-      return;
-    }
-
-    className = className.split(" ");
-
-    DomUtils.getElements($element).forEach(($this) => {
-      $this.classList.add(...className);
-    });
+// getm more visible sides
+function getMoreVisibleSides($element) {
+  if (!$element) {
+    return {};
   }
 
-  // Remove class for specified element
-  static removeClass($element, className) {
-    if (!$element) {
-      return;
-    }
+  const boxRect = $element.getBoundingClientRect();
+  const availableWidth = window.innerWidth;
+  const availableHeight = window.innerHeight;
 
-    className = className.split(" ");
+  const leftArea = boxRect.left;
+  const topArea = boxRect.top;
+  const rightArea = availableWidth - leftArea - boxRect.width;
+  const bottomArea = availableHeight - topArea - boxRect.height;
 
-    DomUtils.getElements($element).forEach(($this) => {
-      $this.classList.remove(...className);
-    });
-  }
+  const horizontal = leftArea > rightArea ? "left" : "right";
+  const vertical = topArea > bottomArea ? "top" : "bottom";
 
-  // Determine if the element contains the specified class
-  static hasClass($element, className) {
-    if (!$element) {
-      return false;
-    }
-
-    return $element.classList.contains(className);
-  }
-
-  // Get element by element or CSS selector
-  static getElement($element) {
-    if ($element) {
-      if (typeof $element === "string") {
-        $element = document.querySelector($element);
-      } else if ($element.length !== undefined) {
-        $element = $element[0];
-      }
-    }
-
-    return $element || null;
-  }
-
-  // Get multi array elements
-  static getElements($element) {
-    if (!$element) {
-      return;
-    }
-
-    if ($element.forEach === undefined) {
-      $element = [$element];
-    }
-
-    return $element;
-  }
-
-  // Add events to elements
-  static addEvent($element, events, callback) {
-    DomUtils.addOrRemoveEvent($element, events, callback, "add");
-  }
-
-  // Remove events to elements
-  static removeEvent($element, events, callback) {
-    DomUtils.addOrRemoveEvent($element, events, callback, "remove");
-  }
-
-  // Add or remove element event
-  static addOrRemoveEvent($element, events, callback, action) {
-    if (!$element) {
-      return;
-    }
-
-    const removeEmptyArray = (array) => {
-      if (!Array.isArray(array) || !array.length) {
-        return [];
-      }
-
-      return array.filter(Boolean);
-    };
-
-    events = removeEmptyArray(events.split(" "));
-
-    events.forEach((event) => {
-      $element = DomUtils.getElements($element);
-
-      $element.forEach(($this) => {
-        if (action === "add") {
-          $this.addEventListener(event, callback);
-        } else {
-          $this.removeEventListener(event, callback);
-        }
-      });
-    });
-  }
-
-  // get scrollable element parents
-  static getScrollableParents($element) {
-    if (!$element) {
-      return [];
-    }
-
-    const $scrollableElements = [window];
-    let $parent = $element.parentElement;
-
-    while ($parent) {
-      const overflowValue = getComputedStyle($parent).overflow;
-
-      if (overflowValue.indexOf("scroll") !== -1 || overflowValue.indexOf("auto") !== -1) {
-        $scrollableElements.push($parent);
-      }
-
-      $parent = $parent.parentElement;
-    }
-
-    return $scrollableElements;
-  }
-
-  /**
-   * convert "maxValue" to "data-max-value"
-   * @param {string} prop
-   */
-  static convertPropToDataAttr(prop) {
-    return prop ? `data-popover-${prop}`.replace(/([A-Z])/g, "-$1").toLowerCase() : "";
-  }
-
-  // getm more visible sides
-  static getMoreVisibleSides($element) {
-    if (!$element) {
-      return {};
-    }
-
-    const boxRect = $element.getBoundingClientRect();
-    const availableWidth = window.innerWidth;
-    const availableHeight = window.innerHeight;
-
-    const leftArea = boxRect.left;
-    const topArea = boxRect.top;
-    const rightArea = availableWidth - leftArea - boxRect.width;
-    const bottomArea = availableHeight - topArea - boxRect.height;
-
-    const horizontal = leftArea > rightArea ? "left" : "right";
-    const vertical = topArea > bottomArea ? "top" : "bottom";
-
-    return {
-      horizontal,
-      vertical,
-    };
-  }
-
-  // get absolute coords of the element
-  static getAbsoluteCoords($element) {
-    if (!$element) {
-      return;
-    }
-
-    const boxRect = $element.getBoundingClientRect();
-
-    const pageX =
-      window.scrollX !== undefined
-        ? window.scrollX
-        : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
-
-    const pageY =
-      window.scrollY !== undefined
-        ? window.scrollY
-        : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-
-    return {
-      width: boxRect.width,
-      height: boxRect.height,
-      top: boxRect.top + pageY,
-      right: boxRect.right + pageX,
-      bottom: boxRect.bottom + pageY,
-      left: boxRect.left + pageX,
-    };
-  }
-
-  // getBoundingClientRect
-  static getCoords($element) {
-    return $element ? $element.getBoundingClientRect() : {};
-  }
-
-  // get data-* attribute value
-  static getData($element, name, type) {
-    if (!$element) {
-      return;
-    }
-
-    let value = $element ? $element.dataset[name] : "";
-
-    if (type === "number") {
-      value = parseFloat(value) || 0;
-    } else {
-      if (value === "true") {
-        value = true;
-      } else if (value === "false") {
-        value = false;
-      }
-    }
-
-    return value;
-  }
-
-  // set data-* attribute value
-  static setData($element, name, value) {
-    if (!$element) {
-      return;
-    }
-
-    $element.dataset[name] = value;
-  }
-
-  // Set element style
-  static setStyle($element, name, value) {
-    if (!$element) {
-      return;
-    }
-
-    $element.style[name] = value;
-  }
-
-  // Display elements through display style
-  static show($element, value = "block") {
-    DomUtils.setStyle($element, "display", value);
-  }
-
-  // Hide elements through display style
-  static hide($element) {
-    DomUtils.setStyle($element, "display", "none");
-  }
-
-  /** getting parent element which could hide absolute positioned child */
-  static getHideableParent($element) {
-    let $hideableParent;
-    let $parent = $element.parentElement;
-
-    while ($parent) {
-      const overflowValue = getComputedStyle($parent).overflow;
-
-      if (overflowValue.indexOf("scroll") !== -1 || overflowValue.indexOf("auto") !== -1) {
-        $hideableParent = $parent;
-        break;
-      }
-
-      $parent = $parent.parentElement;
-    }
-
-    return $hideableParent;
-  }
+  return {
+    horizontal,
+    vertical,
+  };
 }
 
-const placementsMapping = [
-  "top",
-  "top-start",
-  "top-end",
-  "bottom",
-  "bottom-start",
-  "bottom-end",
-  "left",
-  "left-start",
-  "left-end",
-  "right",
-  "right-start",
-  "right-end",
-];
+// get absolute coords of the element
+function getAbsoluteCoords($element) {
+  if (!$element) {
+    return;
+  }
 
-// const placementsClass = placementsMapping.map((d) => `placement-${d}`);
+  const boxRect = $element.getBoundingClientRect();
 
-// const arrowRotateMapping = {
-//   top: "rotate(180deg)",
-//   "top-start": "rotate(180deg)",
-//   "top-end": "rotate(180deg)",
-//   left: "rotate(90deg)",
-//   "left-start": "rotate(90deg)",
-//   "left-end": "rotate(90deg)",
-//   right: "rotate(-90deg)",
-//   "right-start": "rotate(-90deg)",
-//   "right-end": "rotate(-90deg)",
-// };
+  const pageX =
+    window.scrollX !== undefined
+      ? window.scrollX
+      : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
 
-const defaultOptions = {
-  placement: "auto",
-  margin: 8,
-  offset: 5,
-  transitionDistance: 10,
-};
+  const pageY =
+    window.scrollY !== undefined
+      ? window.scrollY
+      : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+  return {
+    width: boxRect.width,
+    height: boxRect.height,
+    top: boxRect.top + pageY,
+    right: boxRect.right + pageX,
+    bottom: boxRect.bottom + pageY,
+    left: boxRect.left + pageX,
+  };
+}
 
 /**
  * compute position
  * @param {*} params
  * @returns {}
  */
-export default function computePosition({
+export default function getPosition({
   // Trigger element
   triggerElement,
   // Popover element
@@ -318,7 +77,7 @@ export default function computePosition({
   }
 
   // popover Rect
-  const popoverElementCoords = DomUtils.getAbsoluteCoords(popoverElement);
+  const popoverElementCoords = getAbsoluteCoords(popoverElement);
   const popoverElementWidth = popoverElementCoords.width;
   const popoverElementHeight = popoverElementCoords.height;
   const popoverElementTop = popoverElementCoords.top;
@@ -327,7 +86,7 @@ export default function computePosition({
   const popoverElementLeft = popoverElementCoords.left;
 
   // trigger Rect
-  const triggerElementCoords = DomUtils.getAbsoluteCoords(triggerElement);
+  const triggerElementCoords = getAbsoluteCoords(triggerElement);
   const triggerElementWidth = triggerElementCoords.width;
   const triggerElementHeight = triggerElementCoords.height;
   const triggerElementTop = triggerElementCoords.top;
@@ -337,7 +96,7 @@ export default function computePosition({
 
   /** find the placement which has more space */
   if (placement === "auto") {
-    const moreVisibleSides = DomUtils.getMoreVisibleSides(triggerElement);
+    const moreVisibleSides = getMoreVisibleSides(triggerElement);
     placement = moreVisibleSides.vertical;
   }
 
@@ -551,34 +310,3 @@ export default function computePosition({
     placement,
   };
 }
-
-// /////// =================
-// const cvtPlacement = {
-//   t: "top",
-//   tl: "top-start",
-//   tr: "top-end",
-//   r: "right",
-//   rt: "right-start",
-//   rb: "right-end",
-//   b: "bottom",
-//   br: "bottom-start",
-//   bl: "bottom-end",
-//   l: "left",
-//   lt: "left-start",
-//   lb: "left-end",
-// } as any;
-
-// const inverseP = {} as any;
-// Object.keys(cvtPlacement).forEach((p) => {
-//   const cr = cvtPlacement[p];
-//   inverseP[cr] = p;
-// });
-
-// const computedPosition = computePosition({
-//   triggerElement: config.trigger,
-//   popoverElement: this.originalElement,
-//   arrowElement: this.arrowElement,
-//   placement: config.placement ? cvtPlacement[config.placement] : "top",
-// });
-// console.log(computedPosition);
-// /////// =================

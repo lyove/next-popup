58fa85c4eb71b72e6f65171e606ed5ef0de8cb6f
Lyove <lyove@outlook.com>
2023-08-31 02:03:17 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -9,21 +9,21 @@ export const PopoverArrowClass = "popover-arrow";
 export const PopoverArrowInnerClass = "arrow";
 
 export enum PLACEMENT {
-  T = "t",
-  TL = "tl",
-  TR = "tr",
-  B = "b",
-  BL = "bl",
-  BR = "br",
-  L = "l",
-  LT = "lt",
-  LB = "lb",
-  R = "r",
-  RT = "rt",
-  RB = "rb",
+  "Top" = "top",
+  "TopStart" = "top-start",
+  "TopEnd" = "top-end",
+  "Bottom" = "bottom",
+  "BottomStart" = "bottom-start",
+  "BottomEnd" = "bottom-end",
+  "Left" = "left",
+  "LeftStart" = "left-start",
+  "LeftEnd" = "left-end",
+  "Right" = "right",
+  "RightStart" = "right-start",
+  "RightEnd" = "right-end",
 }
 
 export enum EmitType {
-  HOVER = "hover",
-  CLICK = "click",
+  Hover = "hover",
+  Click = "click",
 }

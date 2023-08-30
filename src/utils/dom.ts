c0f556792663877e7867e52c7cfe76e839bad643
Lyove <lyove@outlook.com>
2023-08-30 14:39:50 +0800

    init

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,60 +1,95 @@
 /**
- * Dom Utils
+ * create dom element
+ * @param param
+ * @returns HTMLElement
  */
+export function $<T extends HTMLElement>({
+  tagName,
+  attributes,
+  children,
+  style,
+}: {
+  tagName?: string;
+  attributes?: { [key: string]: any };
+  children?: string | Array<Node>;
+  style?: Partial<CSSStyleDeclaration>;
+}): T {
+  const element = document.createElement(tagName || "div");
 
-export function $<T extends HTMLElement>(
-  tagName?: string,
-  attrs?: { [key: string]: any },
-  children?: string | Array<Node>,
-  classPrefix = "",
-): T {
-  let match: string[] = [];
+  // attributes of Boolean type
+  const booleanTypes = ["disabled", "checked", "selected"];
 
-  if (tagName) {
-    match = /([\w-]+)?(?:#([\w-]+))?((?:\.(?:[\w-]+))*)/.exec(tagName) || [];
-  }
-
-  const el = document.createElement(match[1] || "div");
-
-  if (match[2]) {
-    el.id = match[3];
-  }
-  if (match[3]) {
-    el.className = match[3].replace(/\./g, ` ${classPrefix}`).trim();
-  }
-
-  if (attrs) {
-    Object.keys(attrs).forEach((name) => {
-      const value = attrs[name];
-      if (value === undefined) {
-        return;
-      }
-
-      if (name === "selected") {
-        if (value) {
-          el.setAttribute(name, "true");
-        }
+  // set attribute
+  Object.entries(attributes || {}).forEach(([key, val]) => {
+    if (val) {
+      if (booleanTypes.includes(key)) {
+        element.setAttribute(key, "true");
       } else {
-        el.setAttribute(name, value);
+        element.setAttribute(key, `${val}`);
       }
-    });
-  }
+    }
+  });
 
+  // set children
   if (children) {
     if (typeof children === "string") {
-      el.innerHTML = children;
+      element.innerHTML = children;
     } else {
-      children.forEach((c) => el.appendChild(c));
+      children.forEach((c) => element.appendChild(c));
     }
   }
+  // set style
+  Object.entries(style || {}).forEach(([key, val]) => {
+    if (typeof key !== "number") {
+      (<any>element.style)[key] = val;
+    }
+  });
 
-  return el as T;
+  return element as T;
 }
 
-export function clearChildren(dom: Element) {
+/**
+ * remove dom element
+ * @param dom
+ */
+export function $clearChildren(dom: Element) {
   while (dom.firstChild) {
     if (dom.lastChild) {
       dom.removeChild(dom.lastChild);
     }
   }
+}
+
+/**
+ * set style
+ */
+export function $setStyle($element: HTMLElement, style: { [key: string]: string }) {
+  if (!$element) {
+    return;
+  }
+
+  Object.entries(style || {}).forEach(([key, val]) => {
+    if (typeof key !== "number") {
+      (<any>$element.style)[key] = val;
+    }
+  });
+}
+
+/**
+ * set data-* attribute value
+ * @param $element HTMLElement
+ * @param name string
+ * @param value any
+ * @returns HTMLElement
+ */
+export function $setData($element: HTMLElement, data: { [key: string]: any }) {
+  if (!$element) {
+    return;
+  }
+
+  Object.entries(data || {}).forEach(([key, val]) => {
+    if (typeof key !== "number") {
+      $element.dataset[key] = val;
+    }
+  });
 }

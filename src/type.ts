1cf679879109ba30ece24f06d9e181e0f7609855
Lyove <lyove@outlook.com>
2023-08-31 02:01:15 +0800

    Update type.ts

--- src/type.ts
+++ src/type.ts
@@ -1,49 +1,35 @@
 import type { EmitType, PLACEMENT } from "./constant";
 
 export interface PopoverConfig {
-  trigger: Element | { getBoundingClientRect: () => Rect };
+  trigger: HTMLElement | { getBoundingClientRect: () => RectInfo };
   content: Node | string;
   mountContainer?: HTMLElement;
   triggerOpenClass?: string;
   wrapperClass?: string;
   placement?: PLACEMENT;
   showArrow?: boolean;
-  arrow?: Node;
   emit?: EmitType;
   clickOutsideClose?: boolean;
+  open?: boolean;
   openDelay?: number;
   closeDelay?: number;
-  open?: boolean;
   disabled?: boolean;
   enterable?: boolean;
-  translate?: number[];
-  autoPlacement?: boolean;
   autoUpdate?: boolean;
-  autoScroll?: boolean;
   animationClass?: string;
-  overflowHidden?: boolean;
-  coverTrigger?: boolean;
   closeOnScroll?: boolean;
-  hideOnInvisible?: boolean;
-  useTriggerPosition?: boolean;
+  // hideOnInvisible?: boolean;
   closeAnimation?: boolean;
   onBeforeEnter?: () => void;
   onEntered?: () => void;
   onBeforeExit?: () => void;
   onExited?: () => void;
-  onBeforePosition?: (positionXY: PositionXY) => void;
   onOpen?: () => void;
   onClose?: () => void;
   onClickOutside?: () => void;
 }
 
-export interface PositionXY {
-  placement: PLACEMENT;
-  xy?: number[];
-  arrowXY?: number[];
-}
-
-export type Rect = {
+export type RectInfo = {
   left: number;
   top: number;
   width: number;
